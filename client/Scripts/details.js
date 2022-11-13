0b04e5a3bf30cd7bbbb8e666161e0905ed0fcd6b
Ming Jie Wang <jacky.wang.ay@gmail.com>
2022-11-13 16:46:28 -0500

    working save and load surveys

--- client/Scripts/details.js
+++ client/Scripts/details.js
@@ -5,6 +5,7 @@ var options = {
 var creator = new SurveyCreator.SurveyCreator
                     ("YourCreatorElement", options);
 var creator = new SurveyCreator.SurveyCreator(options);
+
 ReactDOM.render (<React.StrictMode>
   <SurveyCreator.SurveyCreatorComponent creator={creator}/>
   
@@ -15,19 +16,14 @@ creator.isAutoSave = true;
 creator.showState = true;
 
 
-
-//change "SaveLoadSurveyCreatorExample" to object id
-var survey_list = "survey";
-
-
-
+var objectId = document.getElementById("id").value;
 
 // Setting this callback will make visible the "Save" button
 creator.saveSurveyFunc = function (saveNo, callback) {
   // save the survey JSON
   console.log(creator.text);
   // You can store in your database JSON as text: creator.text  or as JSON: creator.JSON
-  window.localStorage.setItem(survey_list, creator.text);
+  window.localStorage.setItem(objectId, creator.text);
   // We assume that we can't get error on saving data in local storage
   // Tells creator that changing (saveNo) saved successfully.
   // Creator will update the status from Saving to saved
@@ -45,6 +41,7 @@ var defaultJSON = {
     }
   ]
 };
-creator.text = window.localStorage.getItem(survey_list) || JSON.stringify(defaultJSON);
+creator.text = window.localStorage.getItem(objectId) || JSON.stringify(defaultJSON);
+
 // If you get JSON from your database then you can use creator.JSON property
 // creator.JSON = yourJSON;
--- client/Scripts/load.js
+++ client/Scripts/load.js
@@ -1,14 +1,12 @@
-const survey = require("../../server/models/survey");
-
-var options = {
-  showLogicTab: true,
-  haveCommercialLicense: true //Add this line
+const options = {
+  showLogicTab: true
 };
-var creator = new survey.SurveyCreator
-                  ("YourCreatorElement", options);
-  ReactDOM.render (<React.StrictMode>
-    <SurveyCreator.SurveyCreatorComponent creator={creator}/>
-  </React.StrictMode>, document.getElementById("creatorElement"));
+var creator = new SurveyCreator.SurveyCreator(options);
+ReactDOM.render (<React.StrictMode>
+  <SurveyCreator.SurveyCreatorComponent creator={creator}/>
+</React.StrictMode>, document.getElementById("creatorElement"));
+
+  var objectId = document.getElementById("id").value;
 
   //change "SaveLoadSurveyCreatorExample" to object id
-  creator.text = window.localStorage.getItem("survey") || JSON.stringify(defaultJSON);
+  creator.text = window.localStorage.getItem(objectId) || JSON.stringify(defaultJSON);
